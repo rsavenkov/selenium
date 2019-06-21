d582ab1efc59797977ed411c00bcf4d4a1950269
rsavenkov <lubajnka@mail.ru>
2019-06-21 13:41:31 +0300

    Selenium tests

---
+++ src/test/java/ru/easyum/selenium/DriverInitializer.java
@@ -0,0 +1,38 @@
+package ru.easyum.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Driver initializer designed as singleton
+ */
+public class DriverInitializer {
+
+    private static final Logger logger = LoggerFactory.getLogger(DriverInitializer.class);
+    private static DriverInitializer instance;
+    private WebDriver driver;
+
+    private DriverInitializer() {
+        System.setProperty("webdriver.chrome.driver", getClass().getResource("/chromedriver.exe").getPath());
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        logger.debug("Driver is successfully initialized!");
+    }
+
+    public static DriverInitializer getInstance() {
+        if (instance == null) {
+            instance = new DriverInitializer();
+        }
+        return instance;
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(WebDriver driver) {
+        this.driver = driver;
+    }
+}
---
+++ src/test/java/ru/easyum/selenium/MySuite.java
@@ -0,0 +1,14 @@
+package ru.easyum.selenium;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import ru.easyum.selenium.tests.MyFirstTest;
+import ru.easyum.selenium.tests.MySecondTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    MyFirstTest.class,
+    MySecondTest.class,
+})
+public class MySuite {
+}
