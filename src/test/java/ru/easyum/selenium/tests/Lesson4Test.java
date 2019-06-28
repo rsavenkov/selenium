0e41cc6576859c8c0d0705db8b12dc3462c6a8aa
rsavenkov <lubajnka@mail.ru>
2019-06-28 13:08:50 +0300

    Tests for 4th lesson - 2

--- src/test/java/ru/easyum/selenium/tests/Lesson4Test.java
+++ src/test/java/ru/easyum/selenium/tests/Lesson4Test.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,7 @@ import ru.easyum.selenium.DriverInitializer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Get values, filling form, popups and etc.
@@ -85,14 +87,56 @@ public class Lesson4Test {
     @Test
     public void popup() throws InterruptedException {
         logger.info("popup");
-        driver.get("C:\\Users\\Ruslan\\Projects\\selenium\\selenium\\" +
-                "trunk\\src\\test\\resources\\alert.html");
+        driver.get("C:\\Users\\Ruslan\\Projects\\selenium\\selenium\\trunk\\src\\test\\resources\\alert.html");
         driver.findElement(By.id("modal-button")).click();
         Thread.sleep(1000);
         Alert alert = driver.switchTo().alert();
         logger.info(alert.getText());
         alert.accept();
         Thread.sleep(2000);
+    }
+
+    @Test
+    public void historyAndLocation() throws InterruptedException {
+        logger.info("history and location");
+        driver.navigate().to("https://ya.ru/");
+        Thread.sleep(1000);
+        driver.navigate().to("https://www.google.com/");
+        Thread.sleep(1000);
+        driver.navigate().back();
+        Thread.sleep(1000);
+        driver.navigate().forward();
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void cookies() throws InterruptedException {
+        logger.info("cookies");
+        driver.get("http://www.example.com");
+
+        Cookie cookie = new Cookie("key", "value");
+        driver.manage().addCookie(cookie);
+
+        Set<Cookie> allCookies = driver.manage().getCookies();
+        for (Cookie loadedCookie : allCookies) {
+            logger.info(String.format("%s -> %s", loadedCookie.getName(), loadedCookie.getValue()));
+        }
+
+        driver.manage().deleteCookieNamed("CookieName");
+        driver.manage().deleteAllCookies();
+    }
+
+    @Test
+    public void gragAndDrop() throws InterruptedException {
+        logger.info("grag and drop");
+        driver.get("C:\\Users\\Ruslan\\Projects\\selenium\\selenium" +
+                "\\trunk\\src\\test\\resources\\dragAndDrop.html");
+        WebElement drag = driver.findElement(By.id("drag"));
+        WebElement drop = driver.findElement(By.id("drop"));
+        Thread.sleep(3000);
+//        (new Actions(driver)).dragAndDrop(drag, drop).build().perform();
+        new Actions(driver).clickAndHold(drag).moveToElement(drop).release().build().perform();
+        Thread.sleep(3000);
     }
 
     @After
